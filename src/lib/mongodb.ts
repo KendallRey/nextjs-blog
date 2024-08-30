f959f49a81f566b6546d6051199c4acb9de1f065
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-08-30 14:46:03 +0800

    added initial mongodb

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,31 @@
+import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
+
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+
+if (!uri) {
+  throw new Error("Add NEXT_PUBLIC_MONGODB_URI to env");
+}
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const mongoClient = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await mongoClient.connect();
+    // Send a ping to confirm a successful connection
+    await mongoClient.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoClient.close();
+  }
+}
+run().catch(console.dir);
+
+export default mongoClient
